8fc73596460870138b85d8da477450b7c16dd2a3
Ben Moore <b3nmoore@gmail.com>
2014-09-30 16:52:28 +0100

    start clustering work

--- R/clusterPrices.R
+++ R/clusterPrices.R
@@ -16,3 +16,26 @@ ggplot(sub, aes(x=yearmon, y=Price/1e3, group=sector)) +
   geom_smooth(method="loess") + geom_point() +
   facet_wrap(~sector, ncol=5, scales="free_y") +
   theme_sdc()
+
+
+head(houses)
+library("reshape2")
+hm <- dcast(houses, sector ~ yearmon, 
+            fun.aggregate=median, value.var="Price")
+
+# length reference, mask out low cells
+hm.mask <- dcast(houses, sector ~ yearmon,  value.var="Price")
+
+image(log10(as.matrix(hm[,-1])+1))
+
+library("gplots")
+library("RColorBrewer")
+
+row.lab <- rep("", ncol(hm)-1)
+row.lab[seq(1, ncol(hm)-1, by=4)] <- colnames(hm)[-1][seq(1, ncol(hm)-1, by=4)]
+bl <- colorRampPalette(brewer.pal(9, "Blues"))(64)
+# uneven breaks, norm distributed prices
+heatmap.2(log10(as.matrix(hm[complete.cases(hm),-1])), 
+          dendrogram="row", trace="none", Colv=NULL,
+          col=bl, labRow=F, labCol=row.lab, srtCol=45)
+
