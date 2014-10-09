2ea20a20ad1deaa4d5bf7c1d33a07b59c7250e40
Ben Moore <b3nmoore@gmail.com>
2014-10-09 10:16:02 +0100

    new report header

--- R/report_viz.R
+++ R/report_viz.R
@@ -1,23 +1,15 @@
+library("ggmap")
 source("R/functions.R")
+set.seed(42)
 
 houses <- loadHouses()
 
-
-library("ggmap")
 map <- get_map(location="London", zoom=8, maptype="toner", color="bw", source="stamen")
 p1 <- ggmap(map, extent="device", darken=.6)
-p1 + geom_point(data=houses[sample(1:nrow(houses), 1e5),], 
-                aes(x=Longitude, y=Latitude, alpha=log10(Price)), col="white") +
-  scale_alpha_continuous(range=c(.01, .2)) +
-  labs(x="", y="") + theme(legend.position="none") 
 
-p1+ geom_point(data=houses[houses$yearmon == levels(houses$yearmon)[ym],], 
-                  aes(x=Longitude, y=Latitude, alpha=log10(log10(Price))), 
-                  col="white", size=.3) + 
-    labs(x="", y="") + 
-    theme(legend.position="none",
-          axis.ticks = element_blank(), axis.text.x = element_blank(),
-          axis.text.y = element_blank()) +
-    annotate("text", x=.8, y=50.5, label=levels(houses$yearmon)[ym], 
-             col="white", family="mono") +
-    scale_alpha_continuous(range=c(.2, .45))
+pdf("plots/report_overview.pdf", 2.5, 2.5)
+p1 + geom_point(data=houses[sample(1:nrow(houses), 1e5),], size=I(.2),
+                aes(x=Longitude, y=Latitude, alpha=log10(Price)), col="white") +
+  scale_alpha_continuous(range=c(.05, .25)) +
+  labs(x="", y="") + theme(legend.position="none") 
+dev.off()
